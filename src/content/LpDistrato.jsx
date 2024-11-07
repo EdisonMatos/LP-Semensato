300b63406ea6c24bd385080d7e8be0664c00d0e0
Edison Matos <edison.matos@live.com>
2024-11-07 16:47:51 -0300

    feat: Finaliza lp distrato

--- src/content/LpDistrato.jsx
+++ src/content/LpDistrato.jsx
@@ -6,9 +6,9 @@ const currentYear = new Date().getFullYear();
 const infos = {
   name: "Lúcio Semenssato Advocacia",
   phone: {
-    ddd: "A Definir",
-    firstPart: "A Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A Definir", // Apenas os 4 últimos números
+    ddd: "14",
+    firstPart: "99810", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "1526", // Apenas os 4 últimos números
   },
   email: "contato", // Email desejado pelo cliente
   domain: "A Definir", // Sem "www"
@@ -43,29 +43,107 @@ const LpContent = {
   },
   features: {
     sectionHeader: {
-      miniTag: "A Definir",
-      title: "A Definir",
+      miniTag: "ENCERRE SEU FINANCIAMENTO",
+      title: "Como encerrar com total segurança?",
     },
     cards: {
       card1: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-chart-column"
+          >
+            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="M8 18v-1" />
+            <path d="M12 18v-6" />
+            <path d="M16 18v-3" />
+          </svg>
+        ),
+        title: "Análise",
+        description:
+          "Exame detalhado do contrato para identificar as condições de devolução e os direitos do cliente, oferecendo uma visão clara das opções disponíveis.",
       },
       card2: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-handshake"
+          >
+            <path d="m11 17 2 2a1 1 0 1 0 3-3" />
+            <path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4" />
+            <path d="m21 3 1 11h-2" />
+            <path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3" />
+            <path d="M3 4h8" />
+          </svg>
+        ),
+        title: "Negociação",
+        description:
+          "Com base na análise contratual, entraremos em contato com a instituição financeira para negociar as condições de devolução do imóvel e possíveis abatimentos nas parcelas pagas.",
       },
       card3: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-lightbulb"
+          >
+            <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
+            <path d="M9 18h6" />
+            <path d="M10 22h4" />
+          </svg>
+        ),
+        title: "Elaboração",
+        description:
+          "Preparamos um termo de distrato, formalizando a devolução do imóvel ou terreno e o encerramento do financiamento de forma legal e segura.",
       },
       card4: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-scale"
+          >
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+            <path d="M7 21h10" />
+            <path d="M12 3v18" />
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2" />
+          </svg>
+        ),
+        title: "Assessoramento Jurídico",
+        description:
+          "Após a assinatura do distrato, continuaremos assessorando o cliente, garantindo que todos os procedimentos sejam cumpridos e que o cliente recupere possíveis valores pagos a mais durante o financiamento.",
       },
     },
   },
@@ -76,53 +154,130 @@ const LpContent = {
       alt: "Logomarca",
     },
     sectionHeader: {
-      miniTag: "A Definir",
-      title: "A Definir",
+      miniTag: "TRADIÇÃO EM DISTRATO IMOBILIÁRIO",
+      title: "Especialista em devolução de imóveis",
     },
     paragraph: (
       <p>
-        A Definir
+        Com anos de experiência no mercado imobiliário, nossa equipe oferece
+        assessoria especializada para quem deseja realizar o distrato de um
+        imóvel financiado.
         <br />
-        <br /> A Definir
+        <br /> Acompanhamos cada passo do processo com atenção e segurança
+        jurídica, desde a análise do contrato até a negociação com a instituição
+        financeira. Confie em nossa experiência para resolver suas pendências e
+        encerrar seu financiamento de forma clara e tranquila.
       </p>
     ),
-    buttonLabel: "A Definir",
-    ctaButtonAriaLabel: "A Definir",
+    buttonLabel: "Falar com um especialista",
+    ctaButtonAriaLabel: "Falar com um especialista",
   },
   whyUs: {
     sectionHeader: {
-      miniTag: "A Definir",
-      title: "A Definir",
+      miniTag: "A MELHOR ESCOLHA",
+      title: "Por que nos escolher pra te defender?",
     },
     cards: {
       card1: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-hourglass"
+          >
+            <path d="M5 22h14" />
+            <path d="M5 2h14" />
+            <path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22" />
+            <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2" />
+          </svg>
+        ),
+        title: "Tradição no mercado",
+        description:
+          "Com anos de atuação no setor jurídico, temos um histórico de sucesso em diversas áreas, oferecendo segurança e confiança para nossos clientes.",
       },
       card2: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-headset"
+          >
+            <path d="M3 11h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-5Zm0 0a9 9 0 1 1 18 0m0 0v5a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3Z" />
+            <path d="M21 16v2a4 4 0 0 1-4 4h-5" />
+          </svg>
+        ),
+        title: "Atendimento exclusivo",
+        description:
+          "Acreditamos que cada cliente merece uma atenção especial. Nosso time oferece um atendimento próximo e exclusivo para garantir que suas necessidades sejam sempre atendidas.",
       },
       card3: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-thumbs-up"
+          >
+            <path d="M7 10v12" />
+            <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z" />
+          </svg>
+        ),
+        title: "Sem burocracia",
+        description:
+          "Nosso compromisso é buscar as soluções mais ágeis e eficazes para resolver seu problema, sempre com foco em resultados satisfatórios e sem complicação.",
       },
       card4: {
-        icon: "A Definir",
-        title: "A Definir",
-        description: "A Definir",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-smile"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+            <line x1="9" x2="9.01" y1="9" y2="9" />
+            <line x1="15" x2="15.01" y1="9" y2="9" />
+          </svg>
+        ),
+        title: "Compromisso",
+        description:
+          "Valorizamos a honestidade e a transparência com nossos clientes, mantendo-os informados sobre cada etapa do processo, sem surpresas.",
       },
     },
   },
   cta: {
     bgImg: bgImg,
     sectionHeader: {
-      miniTag: "A Definir",
-      title: "A Definir",
-      subtitle: `A Definir`,
+      miniTag: "NÃO PERCA MAIS TEMPO",
+      title: "Encerre seu financiamento imobiliário agora mesmo!",
+      subtitle: `O escritório Lúcio Semenssato Advocacia é especializado em distrato imobiliário e está pronto para defender seus direitos. Com nossa orientação jurídica, garantimos que o processo de devolução do imóvel e o encerramento do financiamento sejam feitos de forma justa e rápida. Não deixe que questões burocráticas ou financeiras atrasem a resolução do seu contrato. Confie na nossa experiência para garantir o encerramento do seu financiamento com segurança.`,
     },
     buttonLabel: "Quero falar com um especialista",
     ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
