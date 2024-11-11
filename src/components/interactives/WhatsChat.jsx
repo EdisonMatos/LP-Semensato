f84d3f4787baf82dddf35da81bd94a4a68917cfe
Gabriel <gabrielsouza65115@gmail.com>
2024-11-11 20:40:30 -0300

    feat: Cria redirecionamento para whatsapp

--- src/components/interactives/WhatsChat.jsx
+++ src/components/interactives/WhatsChat.jsx
@@ -2,29 +2,49 @@ import { FloatingWhatsApp } from "@carlos8a/react-whatsapp-floating-button";
 import imgProfilePicture from "../../assets/imgs/about/profile.jpg";
 import "../../../src/index.css";
 
-export default function WhatsChat() {
+export default function WhatsChat({ noChat }) {
   return (
     <div>
-      <FloatingWhatsApp
-        status="w-3"
-        chatboxHeight="auto"
-        phoneNumber="5514998101526" // Required
-        notificationDelay={5}
-        accountName="Dr. Lúcio Semenssato" // Optional
-        avatar={imgProfilePicture} // Optional
-        initialMessageByServer="Gostaria de falar com um Advogado Trabalhista agora mesmo?" // Optional
-        initialMessageByClient="Hello! I found your contact on your website. I would like to chat with you about..." // Optional
-        statusMessage="Disponível" // Optional
-        startChatText="Falar no whatsapp" // Optional
-        tooltipText={
-          <p className="text-paragraph4">
-            Atendimento
-            <strong> 24h</strong>
-          </p>
-        } // Optional
-        allowEsc={true} // Optional
-        // Explore all available props below
-      />
+      {noChat ? (
+        <FloatingWhatsApp
+          status="w-3"
+          chatboxHeight="auto"
+          phoneNumber="5514998101526"
+          notificationDelay={5}
+          accountName="Dr. Lúcio Semenssato"
+          avatar={imgProfilePicture}
+          initialMessageByServer="Gostaria de falar com um Advogado Trabalhista agora mesmo?"
+          initialMessageByClient="Hello! I found your contact on your website. I would like to chat with you about..."
+          statusMessage="Disponível"
+          startChatText="Falar no whatsapp"
+          tooltipText={
+            <p className="text-paragraph4">
+              Atendimento<strong> 24h</strong>
+            </p>
+          }
+          allowEsc={true}
+          className="floating-chat"
+        />
+      ) : (
+        <a
+          href="https://wa.me/5514998101526"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="floating-chat whatsapp-redirect"
+        >
+          <FloatingWhatsApp
+            phoneNumber="5514998101526"
+            accountName="Dr. Lúcio Semenssato"
+            avatar={imgProfilePicture}
+            tooltipText={
+              <p className="text-paragraph4">
+                Atendimento<strong> 24h</strong>
+              </p>
+            }
+            chatboxHeight={0}
+          />
+        </a>
+      )}
     </div>
   );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -31,7 +31,7 @@ export default function Index() {
       <Steps />
       <Faq />
       <FooterSocial obs={false} addres={false} />
-      <WhatsChat />
+      <WhatsChat noChat={false}/>
       {/* <FloatingWhatsappButton /> */}
       <BackToTopButton />
     </div>
