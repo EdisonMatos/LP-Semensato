51b5ccf845c2bfab5f6211b8b5ea4c9e33aae1e7
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-01 14:23:38 -0300

    fix: Copy alterado

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -93,7 +93,7 @@ export const infos = {
   whatsappDefaultMessage:
     "Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.",
   whatsChatDefaultMessage:
-    "Gostaria de falar com um advogado trabalhista agora mesmo? Clque abaixo 👇",
+    "Gostaria de falar com um advogado agora mesmo? Clique abaixo 👇",
   slogan: "",
 };
 
